61c57e4deb8ae1a825045c92fb6e884ec1ce0ccf
Ryan Mahapatra <70539418+rym1826@users.noreply.github.com>
2020-10-14 09:32:26 -0500

    Add files via upload

---
+++ bad_income.js
@@ -0,0 +1,66 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+var calculateIncome = function () 
+{
+    var floatAnnualIncome = parseInt($("annualIncome").value);
+    var intUserAge = parseInt($("age").value);
+    var floatTaxRate, floatAnnualTax;
+    if (intUserAge >= 65)
+
+    	if (floatAnnualIncome <= 30000)
+    	{
+    		floatTaxRate = 2.5;
+        }
+    	else
+    	{
+    		if(floatAnnualIncome >= 30000 || floatAnnualIncome <= 75000)
+    		{
+    			floatTaxRate = 5;
+    		}
+    		else
+    	{
+    			floatTaxRate = 20;
+    	}
+    } 
+    else
+    {
+    	
+    	if (floatAnnualIncome <= 30000)
+    	{
+    		floatTaxRate = 5;
+    	}
+        else
+        {
+            if (floatAnnualIncome >= 30000 && floatAnnualIncome <= 75000)
+            {
+   			floatTaxRate = 10;
+            }
+            else
+   		{
+   			floatTaxRate = 20;
+   		}
+	    }
+
+
+	floatAnnualTax = floatAnnualIncome * (floatTaxRate/100);
+
+    }	
+   
+    $("annualTax").value = floatAnnualTax;
+
+
+
+
+};
+
+window.onload = function () 
+{
+    $("annualIncome").value = "";
+    $("age").value = "";
+    $("annualTax").value = "";
+    $("calculate").onclick = calculateIncome;
+    $("annualIncome").focus();
+}
